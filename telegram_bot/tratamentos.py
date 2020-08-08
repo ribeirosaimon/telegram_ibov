c04a3020545fa97ab94e1bbe73fc664ea91d550d
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-08 12:50:53 -0300

    comit

--- telegram_bot/tratamentos.py
+++ telegram_bot/tratamentos.py
@@ -1,2 +1,4 @@
 def tratamentos(acao_json):
-    print(acao_json)
+    analise_fundamentalista=acao_json['fundamentalist_analysis']
+    fechamento = analise_fundamentalista['adj_close']
+    print(fechamento)
