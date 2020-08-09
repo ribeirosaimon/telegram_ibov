a6a10df3cb55a518cc103cdf27f6f38b71fd3a0b
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-09 12:01:18 -0300

    comit

--- tratamentos_das_acoes/tratamentos.py
+++ tratamentos_das_acoes/tratamentos.py
@@ -5,11 +5,11 @@ from api_cotacao_bolsa.pegando_cotacao import *
 
 lista_de_mensagens = []
 
-def tratamentos(context):
+def tratamentos(chat_id):
     #chat_id=context.job.context
     carteira_acao = pesquisar_carteira(chat_id)
     for acao in carteira_acao:
-        nome_acao_da_carteira = acao[0]
+        nome_acao_da_carteira = acao[0].upper()
         preco_acao_da_carteira = acao[1]
         api_rest=buscar_json_da_acao(nome_acao_da_carteira)
         if preco_acao_da_carteira <= api_rest['fundamentalist_analysis']['adj_close']:
@@ -18,5 +18,9 @@ def tratamentos(context):
             lista_de_mensagens.append(resposta)
         if api_rest['fundamentalist_analysis']['adj_close'] >= api_rest['fundamentalist_analysis']['high']:
             resposta = f'Sua Ação {nome_acao_da_carteira} está na máxima do dia'
+            print(resposta)
+            lista_de_mensagens.append(resposta)
+        if api_rest['fundamentalist_analysis']['adj_close'] <= api_rest['fundamentalist_analysis']['low']:
+            resposta = f'Sua Ação {nome_acao_da_carteira} está na mínima do dia'
             print(resposta)
             lista_de_mensagens.append(resposta)
