00454a820af9032ced952909979c3959a3f49a6c
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-08 22:11:18 -0300

    Update tratamentos.py

--- tratamentos_das_acoes/tratamentos.py
+++ tratamentos_das_acoes/tratamentos.py
@@ -3,11 +3,15 @@
 from api_crud_carteira.carteira import pesquisar_carteira
 from api_cotacao_bolsa.pegando_cotacao import *
 
-def tratamentos(id_usuario):
-    carteira_acao = pesquisar_carteira(id_usuario)
+lista_de_mensagens = []
+
+def tratamentos(context):
+    #chat_id=context.job.context
+    carteira_acao = pesquisar_carteira(chat_id)
     for acao in carteira_acao:
         nome_acao_da_carteira = acao[0]
         preco_acao_da_carteira = acao[1]
         api_rest=buscar_json_da_acao(nome_acao_da_carteira)
         if preco_acao_da_carteira <= api_rest['fundamentalist_analysis']['adj_close']:
-            print(nome_acao_da_carteira)
+            resposta = f"Sua Ação {nome_acao_da_carteira} está com o preço maior que o indicado"
+            lista_de_mensagens.append(resposta)
