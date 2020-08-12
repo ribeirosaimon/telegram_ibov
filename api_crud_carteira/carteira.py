b768d3362e96024c4b27ac1dfd94a8456e6fb4a2
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-12 15:54:22 -0300

    comit

--- api_crud_carteira/carteira.py
+++ api_crud_carteira/carteira.py
@@ -6,16 +6,16 @@ from api_crud_carteira.sites_infos import *
 url_base = HEROKU_API_CRUD
 
 url_base = HEROKU_API_CRUD
-criar_acao = url_base + '/carteira/'
-del_acao = url_base +'/usuarios/'
-info_carteira = url_base + '/usuarios/'
+criar_acao = url_base + '/carteira'
+del_acao = url_base +'/carteira'
+info_carteira = url_base + '/usuarios'
 
 def adicionar_acao(id_usuario, acao, preco_medio):
-    new_id = str(datetime.now()).replace('-','').replace(':','').replace('.','').replace(' ','')[::-1][:10]
-    endpoint = f'{criar_acao}{new_id}/{str(id_usuario)}'
-    add_acao = {'acao':str(acao),
-                'preco_medio':preco_medio,
-                'usuario':int(id_usuario)
+    new_acao_id = str(datetime.now()).replace('-','').replace(':','').replace('.','').replace(' ','')[::-1][:10]
+    endpoint = f'{criar_acao}/{new_acao_id}'
+    add_acao = {'acao':f'{acao}',
+                'preco_medio':f'{preco_medio}',
+                'usuario':f'{id_usuario}'
                 }
     add_acao_carteira = requests.request('POST', endpoint, json=add_acao)
     return add_acao_carteira
@@ -24,17 +24,17 @@ def adicionar_acao(id_usuario, acao, preco_medio):
 def deletar_acao(id_usuario, acao):
     acao = acao.lower()
     id_usuario = int(id_usuario)
-    endpoint = f'{del_acao}/{str(id_usuario)}'
+    endpoint = f'{del_acao}/{id_usuario}'
     resposta = requests.request('GET', endpoint)
     resposta_json = resposta.json()
     acao_id = resposta_json['carteira'][0]['acao_id']
-    endpoint = criar_acao + str(acao_id)
+    endpoint = f'{criar_acao}/{acao_id}'
     deletar_acao = requests.request('DELETE', endpoint)
     return deletar_acao
 
 def pesquisar_carteira(id_usuario):
     lista_retorno = []
-    endpoint = f'{info_carteira}/{str(id_usuario)}'
+    endpoint = f'{info_carteira}/{id_usuario}'
     resposta = requests.request('GET', endpoint)
     carteira_json = resposta.json()
     carteira_json = carteira_json['carteira']
--- api_crud_carteira/criar_carteira_db.py
+++ api_crud_carteira/criar_carteira_db.py
@@ -3,10 +3,10 @@ import json
 from api_crud_carteira.sites_infos import *
 
 url_base = HEROKU_API_CRUD
-criar_carteira = url_base + '/usuarios/'
+criar_carteira = url_base + '/usuarios'
 
 def criar_carteira_db(id_telegram):
-    endpoint = criar_carteira + str(id_telegram)
-    usuario_id = {'usuario':int(id_telegram)}
+    endpoint = f'{criar_carteira}/{id_telegram}'
+    usuario_id = {'usuario':f'{id_telegram}'}
     resposta_usuario = requests.request('POST', endpoint, json=usuario_id)
     return resposta_usuario
