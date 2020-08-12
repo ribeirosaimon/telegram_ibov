5e3800d6a74b38c149a7906ca2f3fcd9bdf784ea
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-12 14:57:50 -0300

    comit

--- api_cotacao_bolsa/sites_infos.py
+++ api_cotacao_bolsa/sites_infos.py
@@ -1,2 +1,2 @@
 HEROKU_API_INFO = 'https://secure-wildwood-34847.herokuapp.com/'
-HEROKU_API_CRUD = 'https://dashboard.heroku.com/apps/api-crud-carteira-ibov'
+HEROKU_API_CRUD = 'https://api-crud-carteira-ibov.herokuapp.com'
