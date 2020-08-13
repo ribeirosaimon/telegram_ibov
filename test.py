cbbb009097c5ac491e29e2c0a974d53ddcd4ae20
Saimon <saimonribeiros@hotmail.com>
2020-08-13 12:58:57 -0300

    testocmit

---
+++ test.py
@@ -0,0 +1,15 @@
+from api_crud_carteira.carteira import *
+from api_crud_carteira.criar_carteira_db import *
+from api_cotacao_bolsa.pegando_cotacao import *
+from tratamentos_das_acoes.tratamentos import *
+from tratamentos_das_acoes.tratamento_hora_do_dia import *
+import time
+
+
+
+id_usuario = 883934505
+acao = 'movi3'
+
+while True:
+    tratamentos(id_usuario)
+    time.sleep(10)
